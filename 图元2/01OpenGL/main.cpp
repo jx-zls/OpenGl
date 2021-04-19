82e154db2d7d3d987fe5dc7bca1c95e9267d401c
mapeng <18291820539@163.com>
2021-04-19 16:45:56 +0800

    图元2

---
+++ 图元2/01OpenGL/main.cpp
@@ -0,0 +1,445 @@
+
+#include "GLTools.h"
+#include "GLMatrixStack.h"
+#include "GLFrame.h"
+#include "GLFrustum.h"
+#include "GLBatch.h"
+#include "GLGeometryTransform.h"
+
+#include <math.h>
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+/*
+ 在Mac 系统下，`#include<glut/glut.h>`
+ 在Windows 和 Linux上，我们使用freeglut的静态库版本并且需要添加一个宏
+*/
+
+//定义一个，着色管理器
+GLShaderManager shaderManager;
+GLMatrixStack modelViewMatrix;
+GLMatrixStack projectionMatrix;
+GLFrame       cameraFrame;
+GLFrame       objectFrame;
+
+GLFrustum viewFrustum;
+
+//容器类（7种不同的图元对应7种容器对象）
+GLBatch                pointBatch;
+GLBatch                lineBatch;
+GLBatch                lineStripBatch;
+GLBatch                lineLoopBatch;
+GLBatch                triangleBatch;
+GLBatch             triangleStripBatch;
+GLBatch             triangleFanBatch;
+
+//几何变换的管道
+GLGeometryTransform    transformPipeline;
+M3DMatrix44f        shadowMatrix;
+
+
+GLfloat vGreen[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+GLfloat vBlack[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+
+// 跟踪效果步骤
+int nStep = 0;
+
+void DrawWireFramedBatch(GLBatch* pBatch)
+{
+    /*------------画绿色部分----------------*/
+    /* GLShaderManager 中的Uniform 值——平面着色器
+     参数1：平面着色器
+     参数2：运行为几何图形变换指定一个 4 * 4变换矩阵
+          --transformPipeline 变换管线（指定了2个矩阵堆栈）
+     参数3：颜色值
+    */
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vGreen);
+    pBatch->Draw();
+    /*-----------边框部分-------------------*/
+    /*
+        glEnable(GLenum mode); 用于启用各种功能。功能由参数决定
+        参数列表：http://blog.csdn.net/augusdi/article/details/23747081
+        注意：glEnable() 不能写在glBegin() 和 glEnd()中间
+        GL_POLYGON_OFFSET_LINE  根据函数glPolygonOffset的设置，启用线的深度偏移
+        GL_LINE_SMOOTH          执行后，过虑线点的锯齿
+        GL_BLEND                启用颜色混合。例如实现半透明效果
+        GL_DEPTH_TEST           启用深度测试 根据坐标的远近自动隐藏被遮住的图形（材料
+     
+     
+        glDisable(GLenum mode); 用于关闭指定的功能 功能由参数决定
+     
+     */
+    
+    //画黑色边框
+    glPolygonOffset(-1.0f, -1.0f);// 偏移深度，在同一位置要绘制填充和边线，会产生z冲突，所以要偏移
+    glEnable(GL_POLYGON_OFFSET_LINE);
+    
+    // 画反锯齿，让黑边好看些
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    //绘制线框几何黑色版 三种模式，实心，边框，点，可以作用在正面，背面，或者两面
+    //通过调用glPolygonMode将多边形正面或者背面设为线框模式，实现线框渲染
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //设置线条宽度
+    glLineWidth(2.5f);
+    
+    /* GLShaderManager 中的Uniform 值——平面着色器
+     参数1：平面着色器
+     参数2：运行为几何图形变换指定一个 4 * 4变换矩阵
+         --transformPipeline.GetModelViewProjectionMatrix() 获取的
+          GetMatrix函数就可以获得矩阵堆栈顶部的值
+     参数3：颜色值（黑色）
+     */
+    
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vBlack);
+    pBatch->Draw();
+
+    // 复原原本的设置
+    ////通过调用glPolygonMode将多边形正面或者背面设为全部填充模式
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glDisable(GL_POLYGON_OFFSET_LINE);
+    glLineWidth(1.0f);
+    glDisable(GL_BLEND);
+    glDisable(GL_LINE_SMOOTH);
+    
+}
+
+/*
+ 在窗口大小改变时，接收新的宽度&高度。
+ */
+void changeSize(int w,int h)
+{
+    /*
+      x,y 参数代表窗口中视图的左下角坐标，而宽度、高度是像素为表示，通常x,y 都是为0
+     */
+    glViewport(0, 0, w, h);
+    viewFrustum.SetPerspective(35.0f, float(w)/float(h), 1.0f, 500.0f);
+    projectionMatrix.LoadMatrix(viewFrustum.GetProjectionMatrix());
+    modelViewMatrix.LoadIdentity();
+    
+}
+
+void RenderScene(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    modelViewMatrix.PushMatrix();
+    
+    M3DMatrix44f mCamera;
+    cameraFrame.GetCameraMatrix(mCamera);
+    modelViewMatrix.MultMatrix(mCamera);
+    
+    M3DMatrix44f mObjectFrame;
+    objectFrame.GetMatrix(mObjectFrame);
+    modelViewMatrix.MultMatrix(mObjectFrame);
+    
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vBlack);
+    switch(nStep) {
+        case 0:
+            //设置点的大小
+            glPointSize(4.0f);
+            pointBatch.Draw();
+            glPointSize(1.0f);
+            break;
+        case 1:
+            //设置线的宽度
+            glLineWidth(2.0f);
+            lineBatch.Draw();
+            glLineWidth(1.0f);
+            break;
+        case 2:
+            glLineWidth(2.0f);
+            lineStripBatch.Draw();
+            glLineWidth(1.0f);
+            break;
+        case 3:
+            glLineWidth(2.0f);
+            lineLoopBatch.Draw();
+            glLineWidth(1.0f);
+            break;
+        case 4:
+            DrawWireFramedBatch(&triangleBatch);
+            break;
+        case 5:
+            DrawWireFramedBatch(&triangleStripBatch);
+            break;
+        case 6:
+            DrawWireFramedBatch(&triangleFanBatch);
+            break;
+    }
+    
+    //还原到以前的模型视图矩阵（单位矩阵）
+    modelViewMatrix.PopMatrix();
+    
+    // 进行缓冲区交换
+    glutSwapBuffers();
+}
+
+
+void SpecialKeys(int key, int x, int y)
+{
+    
+    if(key == GLUT_KEY_UP)
+        //围绕一个指定的X,Y,Z轴旋转。
+        objectFrame.RotateWorld(m3dDegToRad(-5.0f), 1.0f, 0.0f, 0.0f);
+    
+    if(key == GLUT_KEY_DOWN)
+        objectFrame.RotateWorld(m3dDegToRad(5.0f), 1.0f, 0.0f, 0.0f);
+    
+    if(key == GLUT_KEY_LEFT)
+        objectFrame.RotateWorld(m3dDegToRad(-5.0f), 0.0f, 1.0f, 0.0f);
+    
+    if(key == GLUT_KEY_RIGHT)
+        objectFrame.RotateWorld(m3dDegToRad(5.0f), 0.0f, 1.0f, 0.0f);
+    
+    glutPostRedisplay();
+}
+
+
+//根据空格次数。切换不同的“窗口名称”
+void KeyPressFunc(unsigned char key, int x, int y)
+{
+    if(key == 32)
+    {
+        nStep++;
+        
+        if(nStep > 6)
+            nStep = 0;
+    }
+    
+    switch(nStep)
+    {
+        case 0:
+            glutSetWindowTitle("GL_POINTS");
+            break;
+        case 1:
+            glutSetWindowTitle("GL_LINES");
+            break;
+        case 2:
+            glutSetWindowTitle("GL_LINE_STRIP");
+            break;
+        case 3:
+            glutSetWindowTitle("GL_LINE_LOOP");
+            break;
+        case 4:
+            glutSetWindowTitle("GL_TRIANGLES");
+            break;
+        case 5:
+            glutSetWindowTitle("GL_TRIANGLE_STRIP");
+            break;
+        case 6:
+            glutSetWindowTitle("GL_TRIANGLE_FAN");
+            break;
+    }
+    
+    glutPostRedisplay();
+}
+
+
+void setupRC()
+{
+    glClearColor(0.7f, 0.7f, 0.7f, 1.0f );
+    shaderManager.InitializeStockShaders();
+    glEnable(GL_DEPTH);
+    transformPipeline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
+    cameraFrame.MoveForward(-20.0f);
+    
+    GLfloat vCoast[24][3] = {
+        {2.80, 1.20, 0.0 }, {2.0,  1.20, 0.0 },
+        {2.0,  1.08, 0.0 },  {2.0,  1.08, 0.0 },
+        {0.0,  0.80, 0.0 },  {-.32, 0.40, 0.0 },
+        {-.48, 0.2, 0.0 },   {-.40, 0.0, 0.0 },
+        {-.60, -.40, 0.0 },  {-.80, -.80, 0.0 },
+        {-.80, -1.4, 0.0 },  {-.40, -1.60, 0.0 },
+        {0.0, -1.20, 0.0 },  { .2, -.80, 0.0 },
+        {.48, -.40, 0.0 },   {.52, -.20, 0.0 },
+        {.48,  .20, 0.0 },   {.80,  .40, 0.0 },
+        {1.20, .80, 0.0 },   {1.60, .60, 0.0 },
+        {2.0, .60, 0.0 },    {2.2, .80, 0.0 },
+        {2.40, 1.0, 0.0 },   {2.80, 1.0, 0.0 }};
+    
+    pointBatch.Begin(GL_POINTS, 24);
+    pointBatch.CopyVertexData3f(vCoast);
+    pointBatch.End();
+    
+    lineBatch.Begin(GL_LINES, 24);
+    lineBatch.CopyVertexData3f(vCoast);
+    lineBatch.End();
+    
+    lineStripBatch.Begin(GL_LINE_STRIP, 24);
+    lineStripBatch.CopyVertexData3f(vCoast);
+    lineStripBatch.End();
+    
+    lineLoopBatch.Begin(GL_LINE_LOOP, 24);
+    lineLoopBatch.CopyVertexData3f(vCoast);
+    lineLoopBatch.End();
+    
+    GLfloat vPyramid[12][3] = {
+        -2.0f, 0.0f, -2.0f,
+        2.0f, 0.0f, -2.0f,
+        0.0f, 4.0f, 0.0f,
+        
+        2.0f, 0.0f, -2.0f,
+        2.0f, 0.0f, 2.0f,
+        0.0f, 4.0f, 0.0f,
+        
+        2.0f, 0.0f, 2.0f,
+        -2.0f, 0.0f, 2.0f,
+        0.0f, 4.0f, 0.0f,
+        
+        -2.0f, 0.0f, 2.0f,
+        -2.0f, 0.0f, -2.0f,
+        0.0f, 4.0f, 0.0f};
+    triangleBatch.Begin(GL_TRIANGLES, 12);
+    triangleBatch.CopyVertexData3f(vPyramid);
+    triangleBatch.End();
+    
+    
+    // 三角形扇形--六边形
+    GLfloat vPoints[100][3];    //超过我们需要的数组
+    int nVerts = 0;
+    //半径
+    GLfloat r = 3.0f;
+    
+    //原点(x,y,z) = (0,0,0);
+    vPoints[nVerts][0] = 0.0f;
+    vPoints[nVerts][1] = 0.0f;
+    vPoints[nVerts][2] = 0.0f;
+    
+    //M3D_2PI 就是2Pi 的意思，就一个圆的意思。 绘制圆形
+    
+    for(GLfloat angle = 0; angle < M3D_2PI; angle += M3D_2PI / 6.0f) {
+        
+        //数组下标自增（每自增1次就表示一个顶点）
+        nVerts++;
+        /*
+         弧长=半径*角度,这里的角度是弧度制,不是平时的角度制
+         既然知道了cos值,那么角度=arccos,求一个反三角函数就行了
+         */
+        //x点坐标 cos(angle) * 半径
+        vPoints[nVerts][0] = float(cos(angle)) * r;
+        //y点坐标 sin(angle) * 半径
+        vPoints[nVerts][1] = float(sin(angle)) * r;
+        //z点的坐标
+        vPoints[nVerts][2] = -0.5f;
+    }
+    
+    //添加闭合的终点
+    //课程添加演示：屏蔽177-180行代码，并把绘制节点改为7.则三角形扇形是无法闭合的。
+    nVerts++;
+    vPoints[nVerts][0] = r;
+    vPoints[nVerts][1] = 0;
+    vPoints[nVerts][2] = 0.0f;
+    
+    // 加载！
+    //GL_TRIANGLE_FAN 以一个圆心为中心呈扇形排列，共用相邻顶点的一组三角形
+    triangleFanBatch.Begin(GL_TRIANGLE_FAN, 8);
+    triangleFanBatch.CopyVertexData3f(vPoints);
+    triangleFanBatch.End();
+    
+    //三角形条带，一个小环或圆柱段
+    //顶点下标
+    int iCounter = 0;
+    //半径
+    GLfloat radius = 3.0f;
+    //从0度~360度，以0.3弧度为步长
+    for(GLfloat angle = 0.0f; angle <= (2.0f*M3D_PI); angle += 0.3f)
+    {
+        //或许圆形的顶点的X,Y
+        GLfloat x = radius * sin(angle);
+        GLfloat y = radius * cos(angle);
+        
+        //绘制2个三角形（他们的x,y顶点一样，只是z点不一样）
+        vPoints[iCounter][0] = x;
+        vPoints[iCounter][1] = y;
+        vPoints[iCounter][2] = -0.5;
+        iCounter++;
+        
+        vPoints[iCounter][0] = x;
+        vPoints[iCounter][1] = y;
+        vPoints[iCounter][2] = 0.5;
+        iCounter++;
+    }
+    
+    // 关闭循环
+    printf("三角形带的顶点数：%d\n",iCounter);
+    //结束循环，在循环位置生成2个三角形
+    vPoints[iCounter][0] = vPoints[0][0];
+    vPoints[iCounter][1] = vPoints[0][1];
+    vPoints[iCounter][2] = -0.5;
+    iCounter++;
+    
+    vPoints[iCounter][0] = vPoints[1][0];
+    vPoints[iCounter][1] = vPoints[1][1];
+    vPoints[iCounter][2] = 0.5;
+    iCounter++;
+    
+    // GL_TRIANGLE_STRIP 共用一个条带（strip）上的顶点的一组三角形
+    triangleStripBatch.Begin(GL_TRIANGLE_STRIP, iCounter);
+    triangleStripBatch.CopyVertexData3f(vPoints);
+    triangleStripBatch.End();
+    
+}
+
+
+int main(int argc,char *argv[])
+{
+    //设置当前工作目录，针对MAC OS X
+    /*
+     `GLTools`函数`glSetWorkingDrectory`用来设置当前工作目录。实际上在Windows中是不必要的，因为工作目录默认就是与程序可执行执行程序相同的目录。但是在Mac OS X中，这个程序将当前工作文件夹改为应用程序捆绑包中的`/Resource`文件夹。`GLUT`的优先设定自动进行了这个中设置，但是这样中方法更加安全。
+     */
+    gltSetWorkingDirectory(argv[0]);
+    
+    
+    //初始化GLUT库,这个函数只是传说命令参数并且初始化glut库
+    glutInit(&argc, argv);
+    
+    
+    glutInitDisplayMode(GLUT_RGBA);
+    
+    //GLUT窗口大小、窗口标题
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("ee");
+    
+    
+    /*
+     GLUT 内部运行一个本地消息循环，拦截适当的消息。然后调用我们不同时间注册的回调函数。我们一共注册2个回调函数：
+     1）为窗口改变大小而设置的一个回调函数
+     2）包含OpenGL 渲染的回调函数
+     */
+    
+    //注册重塑函数
+    glutReshapeFunc(changeSize);
+    glutKeyboardFunc(KeyPressFunc);
+    //注册显示函数
+    glutDisplayFunc(RenderScene);
+
+    glutSpecialFunc(SpecialKeys);
+    
+    /*
+     初始化一个GLEW库,确保OpenGL API对程序完全可用。
+     在试图做任何渲染之前，要检查确定驱动程序的初始化过程中没有任何问题
+     */
+    GLenum status = glewInit();
+    if (GLEW_OK != status) {
+        
+        printf("GLEW Error:%s\n",glewGetErrorString(status));
+        return 1;
+        
+    }
+    
+    
+    //设置我们的渲染环境
+    setupRC();
+    
+    glutMainLoop();
+    
+    return  0;
+    
+}
